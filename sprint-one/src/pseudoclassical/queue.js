65628da7db1e49c5758013ed0e6abfc1f38d653e
adamjung <ajung91@hotmail.com>
2016-11-02 16:31:47 -0700

    Completed working version of Pseudoclassical - Queue.js Completed Data Structure Sprint Part 1

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
+  this.front = 0;
+  this.back = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.back] = value;
+  this.back++;
+  this.count++;
+};
+Queue.prototype.dequeue = function() {
+  var result;
+  if (this.count > 0) {
+    this.count--;
+    result = this.storage[this.front];
+    this.front++;
+  }
+  return result;
+};
+Queue.prototype.size = function() {
+  return this.count;
 };
 
 
