e2f7cff0caea16c05e5abddde08455aa4f17b470
Tim Chin <accounts@hackreactor.com>
2016-11-03 10:36:09 -0700

    Completed working version of LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newTail = new Node(value);
+    if (list.tail !== null) { 
+      // non-empty list, link nextail to the list
+      list.tail.next = newTail;
+    }
+    // move tail to the newNode
+    list.tail = newTail;
+    if (list.head === null) { 
+      // empty list, 
+      list.head = list.tail;
+    }
   };
 
   list.removeHead = function() {
+    var oldHead = list.head;
+    if (list.head !== null) {
+      delete list.head;
+      list.head = oldHead.next;
+    }
+    console.log(list);
+    return oldHead.value;
   };
 
   list.contains = function(target) {
+    var containsHelper = function(node, found) {
+      if (node.value === target) {
+        // base case : target found
+        return true;
+      } else if (node.next === null) { // base case : end of list, not found
+        return false;
+      } else { // recurse
+        return containsHelper(node.next, false);
+      }
+    };
+
+    return containsHelper(list.head, false);
   };
 
   return list;
