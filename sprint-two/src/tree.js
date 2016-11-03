e0baa7db6406b23a35bef766ef1ca236f36e2d26
Tim Chin <accounts@hackreactor.com>
2016-11-03 15:42:05 -0700

    Finished working version of tree.js with 3 extra tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +11,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChild = Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  var containsHelper = function(node, target) {
+    if (node.value === target) { // check current node's value
+      return true;
+    }
+    if (node.children.length === 0) { //Base case
+      return false;
+    }
+
+    for (var i = 0; i < node.children.length; i++) {
+      if (containsHelper(node.children[i], target)) {
+        return true;
+      }
+    }
+    return false;
+  };
+  return containsHelper(this, target, false);
 };
 
 
