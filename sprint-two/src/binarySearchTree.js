cd5d09de5893021b8a542021ea8a64f02ab5d6dd
Hack Reactor Students <accounts@hackreactor.com>
2016-11-04 16:40:36 -0700

    Progress on Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,43 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(treeMethods);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
 };
+
+var treeMethods = {
+  insert: function(value) {
+    var helper = function(node) {
+      if (value < node.value) {
+        // check left
+        if (node.left === null) {
+          node.left = BinarySearchTree(value);
+        } else {
+          // recurse left
+          helper(node.left);
+        }
+      } else if (value > node.value) {
+        // check right
+        if (node.right === null) {
+          node.right = BinarySearchTree(value);
+        } else {
+          // recurse right
+          helper(node.right);
+        }
+      }      
+    };
+    helper(this);
+  },
+  contains: function(value) {
+
+  },
+  depthFirstLog: function() {
+
+  }
+};
+
+
 
 
 /*
